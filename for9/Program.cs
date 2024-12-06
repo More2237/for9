44be254211a2db1abf60d001e7a60a9757439c79
More2237 <noorallyrahmonov2308@gmail.com>
2024-12-06 22:27:30 +0500

    kjhgf

---
+++ for9/Program.cs
@@ -0,0 +1,11 @@
+﻿var a = double.Parse(Console.ReadLine());
+var b = double.Parse(Console.ReadLine());
+double c= 0;
+
+for (double i = a; i <= b; i++ )
+{
+    
+    c += i*i;
+    
+}
+Console.WriteLine(c);
